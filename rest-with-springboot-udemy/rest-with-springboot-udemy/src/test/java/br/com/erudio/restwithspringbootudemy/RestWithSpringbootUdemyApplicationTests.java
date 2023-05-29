217f282c8a8e4373ae57cb9607af88050c12db8c
Thais Rodrigues <71556315+thaisNY@users.noreply.github.com>
2023-05-28 21:49:40 -0300

    --

---
+++ rest-with-springboot-udemy/rest-with-springboot-udemy/src/test/java/br/com/erudio/restwithspringbootudemy/RestWithSpringbootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restwithspringbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
